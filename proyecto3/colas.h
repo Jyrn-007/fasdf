8af156d020e8183d6e8c44904f392bb20e2efef7
Jyrn-007 <uomashu14@gmail.com>
2025-03-29 04:10:50 -0600

    cola

---
+++ proyecto3/colas.h
@@ -0,0 +1,38 @@
+#ifndef COLAS_H
+#define COLAS_H
+#include "NordSecurity.h"
+#include <iostream>
+
+#include <queue>
+#include <string>
+
+using namespace std;
+
+// Estructura para almacenar la evaluación de riesgos
+struct STV{
+    string PlanRutas;
+    string DistRecorrer;
+    string Tráfico;
+    int TiemLlegada;
+    int TiempoRuta;
+    int CapMaximaPasajeros;
+    int CostoCombustible;
+    int HrsTotalesConductores;
+};
+
+// Clase que maneja la cola de evaluaciones de riesgo
+class GestorRiesgos {
+private:
+    queue<STV> colaRiesgos; // Cola de evaluaciones
+  
+public:
+    // Métodos para la gestión de la cola
+    void insertarDatoCola(const STV& riesgo);
+    void mostrarDatosCola() const;
+    void eliminarDatoCola();
+    void ingresarDatosCola();
+    int menuCola();
+   
+};
+
+#endif
